87815186cb2228ad9d9dd1252c57562641e7619c
little512 <skidmanxdd@gmail.com>
2021-01-08 01:14:50 -0600

    change naming convention

--- src/VM/Instruction.cs
+++ src/VM/Instruction.cs
@@ -6,17 +6,17 @@ namespace VM.Components {
         public int InstructionPointer { get; set; }
         public bool Active { get; set; }
         public int ErrorCode { get; set; }
-        private MemoryComponent _memory;
+        private MemoryComponent memory;
 
         public InstructionComponent(RegisterComponent registers, StackComponent stack, MemoryComponent memory) {
             Interpreter = new(registers, stack, memory, this);
             Active = true;
-            _memory = memory;
-            InstructionPointer = _memory.ExecutionStartAddress;
+            this.memory = memory;
+            InstructionPointer = memory.ExecutionStartAddress;
         }
 
         public void Update() {
-            byte Instruction = _memory.Read(InstructionPointer);
+            byte Instruction = memory.Read(InstructionPointer);
 
             InstructionPointer = Interpreter.Execute(Instruction, InstructionPointer);
         }
